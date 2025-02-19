ba7d5430f15d50e91ecbb4d01f17550f32edddae
DianaOlivaresBarahona <olivaresbarahona@live.se>
2025-02-19 14:04:32 +0100

    add header

---
+++ src/header/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <section className="header-box">
+      <section className="header-top__logo">
+        <a href="" className="header-logo">
+          LOGO
+        </a>
+      </section>
+
+      <hr className="header-top__seperator" />
+    </section>
+  );
+};
+
+export default Header;
